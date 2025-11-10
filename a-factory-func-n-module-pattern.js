a52e19501c68b175d035cc6c44872042476ea097
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2025-11-10 16:48:30 -0500

    private variables n functions with factories

--- a-factory-func-n-module-pattern.js
+++ a-factory-func-n-module-pattern.js
@@ -30,8 +30,8 @@ function makeAdding(firstNumber){
 // const add5 = makeAdding(5);
 // console.log(add)
 
-const add5 = makeAdding(5);
-console.log(add5(2)) // logs 7
+// const add5 = makeAdding(5);
+// console.log(add5(2)) // logs 7
 
 // ------------------------ Factory Functions ------------------------------
 
@@ -52,13 +52,56 @@ function createUserShortHand(name){
     const vName = "bob";
     const vAge = 28;
     const vColor = "red";
-    return {vName: vName, vAge: vAge, vColor: vColor};
+    // old pattern:
+    // return {vName: vName, vAge: vAge, vColor: vColor};
+    // new pattern: when the variable has the same name it doesn't need to repeat
+    return {vName, vAge, vColor};
 }
 
+const name = "bob";
+const age = 28;
+const color = "red";
 
-const thatObj = {vName: vName, vAge: vAge, vColor: vColor};// before the shorhand
 
-const nowFancyObj = {vName, vAge, vColor};
+ const thatObj = {name: name, age: age, color: color};// before the shorhand
+ const nowFancyObj = {name, age, color};// after the handshort
 
-console.log()
+// console.log(thatObj);
+// console.log(nowFancyObj);
 
+// ------------------- Destructuring -------------------
+
+const obj = {a: 1, b: 2};
+const {a, b} = obj;
+// console.log(a);
+// console.log(b);
+const array = [9,8,7,6,5,4,3,2,1,0]
+const [lastNumber, ...rest] = array;
+// console.log(lastNumber, rest);
+
+// ---------------------------------- Private variable and functions -----------------------------
+
+function createUserFull(name){
+    const discordName = `@${name}`;
+    
+    // variable reputation is private because it is not returned.
+    // same thing can be done with methods
+    let reputation = 0;
+
+    const getReputation = () => reputation;
+    const giveReputation = () => reputation++;
+
+    return {name, discordName, getReputation, giveReputation};
+}
+
+const josh = createUser("josh");
+josh.giveReputation();
+josh.giveReputation();
+
+console.log({
+  discordName: josh.discordName,
+  reputation: josh.getReputation()
+});
+// logs { discordName: "@josh", reputation: 2 }
+
+// ----------------------- Prototype inheritance with factories
