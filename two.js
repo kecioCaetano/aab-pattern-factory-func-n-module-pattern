057a23f497ef7a4ecabb5f1693f00524a7be753c
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2025-11-13 05:57:45 -0500

    using ES6 Modules

--- two.js
+++ two.js
@@ -1,4 +1,4 @@
 // ------------------- named import ------------------------------
-import greeting, { farewell } from "./one";
+import greeting, { farewell } from './one.js';
 
 console.log(greeting);
